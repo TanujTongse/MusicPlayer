1fa47c48c5340a80595f9f79d63218c7bf24b333
TanujTongse <tanujtongse@gmail.com>
2024-09-14 15:59:49 +0530

    new;

---
+++ src/music.js
@@ -0,0 +1,42 @@
+let allMusic = [
+    {
+        name: "Aap Ki Kashish",
+        artist: "Himesh Rashmiyan",
+        img: "music1",
+        src: "music1"
+    },
+
+    {
+        name: "Akela Hai Mr. Khiladi",
+        artist: "Udit Narayan",
+        img: "music2",
+        src: "music2"
+    },
+
+    {
+        name: "Ankhiyon Ke Jhar",
+        artist: "cover",
+        img: "music3",
+        src: "music3"
+    },
+
+    {
+        name: "Ankhon Mein Basa Lunga",
+        artist: "Mohit",
+        img: "music4",
+        src: "music4"
+    },
+	{
+        name: "Pachtaonge Arijit Singh",
+        artist: "Jani, bPraak",
+        img: "music5",
+        src: "music5"
+    },
+    {
+        name: "Agar Tum Mil Jao",
+        artist: "Female",
+        img: "music6",
+        src: "music6"
+    }
+    
+]
